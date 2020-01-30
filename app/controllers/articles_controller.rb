aabb4eea2a6ec2b0d3a89d8b87d941ab7d2a179d
erick19801 <holasoyerick19801@gmail.com>
2020-01-30 16:16:01 -0500

    finish edit, beging delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,12 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update]
   def new
     @article = Article.new
   end
   def create
     @article = Article.new(article_params)
     if @article.save
-      redirect_to @article
+      redirect_to @article, notice: "Article #{@article.title} created"
     else
       redirect_ to :new
     end
@@ -16,8 +17,20 @@ class ArticlesController < ApplicationController
   def index
     @articulos = Article.all
   end
+  def edit
+  end
+  def update
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
   private
   def article_params
     params.require(:article).permit(:title, :body)
+  end
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-
+    @articulos = Article.all
   end
   def contactos
     @email = 'erick@gmail.com'
